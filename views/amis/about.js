81071d8971c1b8a99ae456a986eb80085d65e97c
LittleYe233 <littleye233@foxmail.com>
2022-04-15 10:06:01 +0800

    feat: add AMIS page for about

---
+++ views/amis/about.js
@@ -0,0 +1,113 @@
+module.exports = {
+  type: 'page',
+  title: 'Hikaru Share Project',
+  body: [
+    {
+      type: 'container',
+      body: [
+        {
+          type: 'container',
+          body: [
+            {
+              type: 'button-toolbar',
+              buttons: [
+                {
+                  type: 'button',
+                  label: 'Home',
+                  actionType: 'url',
+                  blank: false,
+                  url: ''
+                },
+                {
+                  type: 'button',
+                  label: 'Torrents',
+                  actionType: 'url',
+                  blank: false,
+                  url: ''
+                },
+                {
+                  type: 'button',
+                  label: 'User Settings',
+                  actionType: 'url',
+                  blank: false,
+                  url: ''
+                },
+                {
+                  type: 'button',
+                  label: 'Help',
+                  actionType: 'url',
+                  blank: false,
+                  url: ''
+                },
+                {
+                  type: 'button',
+                  label: 'About',
+                  actionType: 'url',
+                  blank: false,
+                  url: ''
+                }
+              ]
+            }
+          ]
+        },
+        {
+          type: 'container',
+          body: [
+            {
+              type: 'tpl',
+              tpl: 'User Information',
+              inline: false,
+              style: {
+                fontWeight: 'bold'
+              }
+            },
+            {
+              type: 'container',
+              body: [],
+              style: {},
+              className: ''
+            }
+          ],
+          style: {
+            marginTop: '',
+            paddingTop: '',
+            borderStyle: 'none',
+            borderColor: '#000000',
+            borderWidth: ''
+          },
+          className: 'm-t'
+        },
+        {
+          type: 'container',
+          body: [
+            {
+              type: 'tpl',
+              tpl: 'About Page',
+              inline: false,
+              wrapperComponent: 'h1',
+              style: {
+                fontWeight: 'bold'
+              }
+            },
+            {
+              type: 'container',
+              body: []
+            }
+          ],
+          style: {
+            textAlign: 'left'
+          },
+          className: 'm-t-lg'
+        }
+      ],
+      style: {
+        textAlign: 'center'
+      },
+      className: 'm-l-lg m-r-lg'
+    }
+  ],
+  messages: {},
+  style: {
+    textAlign: 'left'
+  }
+};
