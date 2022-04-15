bce0d3362aa0babec0d41723ca69fd361bb8befa
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-04-15 16:39:24 +0800

    revert: refactor: move EJS view templates to another folder
    This reverts commit f8d8ed5017a11bb8a8e6505567fec919beb7a0cb.
    
    We found that EJS engine worked badly with external pure JS files. We
    determined to revert the previous commit and put the AMIS files into
    static folder.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views/ejs'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
